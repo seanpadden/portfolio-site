37601ec4ed58fb675d6cc8151fab748d6d0cbaab
Sean Padden <sean.padden7@gmail.com>
2019-10-22 17:22:45 -0400

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Home from './containers/Home.js'
+import { Router, Route, Switch } from "react-router";
 
 class App extends Component {
   render(){
