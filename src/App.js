d23e569b64f465e8a87d2aeba2e89a0283648554
Sean Padden <sean.padden7@gmail.com>
2019-10-22 17:27:28 -0400

    starting skeleton

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 import Home from './containers/Home.js'
 import { Router, Route, Switch } from "react-router";
+import Navbar from './components/Navbar.js'
+import Projects from './components/Projects.js'
 
 class App extends Component {
   render(){
     return (
       <div >
+        <Navbar/>
         <Home />
+        <Projects/>
       </div>
     )
   }
