e7cd73b6d32773f6e2c150a8999ec7f02e405b70
Sean Padden <sean.padden7@gmail.com>
2019-10-22 21:21:18 -0400

    "done": i"ok"t push git push "ok" ":wq"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-import Projects from './Projects'
+import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom'
+
 
 class Navbar extends Component {
   render(){
     return (
-      <div>
-        dill
+      <div className="navbar">
+        <Link to={'/about'}>About</Link>
+        <Link to={'/projects'}>Projects</Link>
+        <Link to={'/contact'}>Contact</Link>
+
       </div>
     )
   }
