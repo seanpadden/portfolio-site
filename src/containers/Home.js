3ea304e854772b2fe653bc8a498ecf662466448c
Sean Padden <sean.padden7@gmail.com>
2019-10-24 19:05:22 -0400

    name on top and changed to link

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import '../Home.css';
+import { Link } from 'react-router-dom'
+
 
 class Home extends Component {
   render(){
     return (
       <div className="Home-component">
         <div style={{fontFamily: `Spectral SC`, color: `white`, fontSize: `40pt`, textAlign: `center`, padding: `20px`}}>
-          Sean Padden
+        <Link to={'/'} style={{color: `white`, textDecoration: `none`}}>Sean Padden</Link>
+
         </div>
       </div>
     )
