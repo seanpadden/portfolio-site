38de7619078ef853ffe414b38a6ec71d4b4eab8e
Sean Padden <sean.padden7@gmail.com>
2019-10-24 19:04:12 -0400

    inline styling and filler text

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class About extends Component {
   render(){
     return (
-      <div>
-        ABOUT ME
+      <div style={{fontFamily: `Spectral SC`, color: `white`, textAlign: `center`}}>
+        Full stack developer specializing in Javascript, React, and Ruby on Rails. 
       </div>
     )
   }
