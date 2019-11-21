65891f049c1701250c46473fd431472ada0eacb7
Sean Padden <sean.padden7@gmail.com>
2019-11-21 13:56:21 -0500

    css fun

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,34 +22,17 @@ class Projects extends Component {
   
   render(){
     return (
-      <div>
-        <div onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
-          <img src={wordmapper} className="project-tile"  />
-          {this.state.hover ?
+        <div className="wrapper" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
+          <img src={wordmapper} className="project-tile" />
+
           <div className="project-text">
             <h1 style={{fontWeight: `900`}} >WordMapper</h1>
             <p>An etymological map for the grammatically curious that explores the history of the English language on a map</p>
-            
           </div>
-          :
-          null
-          }
+
         </div>
-      </div>
     )
   }
-
-//   render(){
-//     return (
-//       <div className="hvrbox">
-//         <img src={wordmapper} alt="WordMapper" />
-//         <div className="hvrbox-layer_top">
-//           <h1 className="hvrbox-text">WordMapper</h1>
-//         </div>
-//       </div>
-//     )
-//   }
-
 }
 
 export default Projects;
