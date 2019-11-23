f2d8c8b02f881b02ae21f609d9751c3343d6dc43
Sean Padden <sean.padden7@gmail.com>
2019-11-23 15:18:57 -0500

    starting contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import SocialIcon from 'react-social-icons'
+import '../App.css'
 
 class Contact extends Component {
   render(){
     return (
-      <div>
-        Contact ME
+      <div className="about" style={{color: `white`, textDecoration: `none`}} >
+        <a href={`mailto:sean.padden7@gmail.com`} style={{color: `white`, textDecoration: `none`}} >sean.padden7@gmail.com</a>
+        <br/>
+        <a href={`https://www.linkedin.com/in/sean-padden/`} style={{color: `white`, textDecoration: `none`}}>LinkedIn</a>
+        <br/>
+        Cell: (631) 241-0485
+        <br/>
+
+
       </div>
     )
   }
