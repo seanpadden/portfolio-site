8ea124af0fb9426f466f253139d94c19ee917dc2
Sean Padden <sean.padden7@gmail.com>
2019-12-02 16:53:34 -0500

    added flex class for project bullets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,13 +10,18 @@ class Projects extends Component {
           <img className="project-img"src={wordmapper} />
           <div className="project-text">
             <h1 style={{fontWeight: `900`}}>WordMapper</h1>
-            <h4>
-              <a className="project-link" href="https://www.github.com/seanpadden/wordmapper-frontend">GitHub</a>
-              <a className="project-link" href="https://wordmapper.surge.sh">Live</a>
-              <a className="project-link" href="https://youtu.be/hWi3tgMjqEU">Demo</a>
-            </h4> 
+              <h4>
+                <a className="project-link" href="https://www.github.com/seanpadden/wordmapper-frontend">GitHub</a>
+                <a className="project-link" href="https://wordmapper.surge.sh">Live</a>
+                <a className="project-link" href="https://youtu.be/hWi3tgMjqEU">Demo</a>
+              </h4> 
             <p>An etymological map for the grammatically curious that explores the history of the English language on a map</p>
           </div>
+        <div className="project-info">
+          <p style={{width: `33%`, marginRight: `3%`}}> Built a dynamic UI with React that allows users to look up words, see their origins rendered on a map, and favorite words and locations</p>
+          <p style={{width: `33%`, marginRight: `3%`}}> Developed Rails API backend to handle RESTful routing with endpoints for word input, maps, and profiles</p>
+          <p style={{width: `33%`}}> Incorporated React-Redux to store global state and handle actions and requests, preventing prop-drilling and optimizing client-side logic</p>
+        </div>
         </div>
     )
   }
